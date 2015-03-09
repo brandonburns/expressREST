ca1ad00cb56c6f1352a929693c29354a1d323730
Brandon Burns <brandon10688@gmail.com>
2015-03-08 22:20:04 -0700

    tests are working

--- test/karma_tests/notes_controller_test.js
+++ test/karma_tests/notes_controller_test.js
@@ -18,7 +18,7 @@ describe('notes controller', function() {
   it('should be able to create a controller', function() {
     var notesController = $ControllerConstructor('notesController', {$scope: $scope});
     expect(typeof notesController).toBe('object');
-    expect(Array.isArray($scope.notes)).toBe(true);
+    expect(Array.isArray($scope.unicorns)).toBe(true);
   });
 
   describe('REST requests', function() {
@@ -27,18 +27,51 @@ describe('notes controller', function() {
     }));
 
     afterEach(function() {
-      $httpBackend.verifyNoOutstandingExpectations();
+      $httpBackend.verifyNoOutstandingExpectation();
       $httpBackend.verifyNoOutstandingRequest();
     });
 
     it('should have a getAll function', function() {
-      $httpBackend.expectGET('/api/v1/unicorns').respond(200, [{unicornName: 'test note'}]);
+      $httpBackend.expectGET('/api/v1/unicorns').respond(200, [{unicornName: 'jim'}]);
 
       var notesController = $ControllerConstructor('notesController', {$scope: $scope});
       $scope.getAll();
       $httpBackend.flush();
 
-      expect($scope.notes[0].unicornName).toBe('test note');
+      expect($scope.unicorns[0].unicornName).toBe('jim');
+    });
+
+    it('should be able to create new unicorn', function() {
+      $httpBackend.expectPOST('/api/v1/unicorns').respond(200, {_id: 1, unicornName: 'bob'});
+
+      var notesController = $ControllerConstructor('notesController', {$scope: $scope});
+      $scope.create({unicornName: 'bob'});
+      $httpBackend.flush();
+
+      expect($scope.unicorns[0]._id).toBe(1);
+    });
+
+    it('should be able to save unicorns', function() {
+      $httpBackend.expectPUT('/api/v1/unicorns/1').respond(200);
+
+      $ControllerConstructor('notesController', {$scope: $scope});
+      var unicorn = {unicornName: 'dude', _id: 1, editing: true};
+      $scope.save(unicorn);
+      $httpBackend.flush();
+
+      expect(unicorn.editing).toBe(false);
+    });
+
+    it('should be able to delete a unicorn', function() {
+      $httpBackend.expectDELETE('api/v1/unicorns/1').respond(200);
+
+      $ControllerConstructor('notesController', {$scope: $scope});
+      var unicorn = {unicornName: 'dude man', _id: 1, editing: true};
+      $scope.unicorns.push(unicorn);
+      $scope.remove(unicorn);
+      $httpBackend.flush();
+
+      expect($scope.unicorns.length).toBe(0);
     });
   });
 });
